11fd6ac94177ce3873f93062b62cd1ad94cdc845
Rowan <rljb@xtra.co.nz>
2022-03-08 21:01:18 +1100

    added .gitignore and changed lots to 20

--- autotrader.py
+++ autotrader.py
@@ -23,7 +23,7 @@ from typing import List
 from ready_trader_go import BaseAutoTrader, Instrument, Lifespan, MAXIMUM_ASK, MINIMUM_BID, Side
 
 
-LOT_SIZE = 10
+LOT_SIZE = 20
 POSITION_LIMIT = 100
 TICK_SIZE_IN_CENTS = 100
 
